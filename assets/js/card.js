4c3d3fa02387ad23c6614bf85c19a10414a9faab
shidqon <shidqon.famulaqih@bukalapak.com>
2018-08-05 23:52:01 +0700

    add card component

---
+++ assets/js/card.js
@@ -0,0 +1,52 @@
+window.SevensCard = {
+  template: `
+    <div class="card" :class="[cardClass, positionClass]"></div>
+  `,
+  props: {
+    type: {
+      type: String,
+      default: '',
+    },
+    num: {
+      type: String,
+      default: '',
+    },
+    flipped: {
+      type: Boolean,
+      default: false,
+    },
+    asDown: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      positionDown: ['2', '3', '4', '5', '6'],
+      positionUp: ['j', 'q', 'k', 'a',],
+    };
+  },
+  watch: {
+    asDown() {
+      if (this.asDown) {
+        this.positionUp.splice(3, 1);
+        this.positionDown.unshift('a');
+      }
+    },
+  },
+  computed: {
+    positionClass() {
+      let posClass = '';
+      if (this.positionDown.includes(this.num)) {
+        posClass = 'card--down';
+      } else if (this.positionUp.includes(this.num)) {
+        posClass = 'card--up'
+      }
+      return posClass;
+    },
+    cardClass() {
+      const cardClass = this.flipped ? 'card--back' : `card--${this.type}--${this.num}`;
+      return cardClass;
+    },
+  },
+};
--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,26 @@
     window.Sevens = new Vue({
       el: '#vm__sevens',
       name: 'Sevens',
+      components: {
+        'card': window.SevensCard,
+      },
       data: {
         types: ['diamonds', 'clubs', 'hearts', 'spades'],
         cards: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a',],
         showList: {
-          diamonds: ['7', 'a'],
+          diamonds: ['6', '7', 'a'],
           clubs: ['7'],
           hearts: ['7'],
           spades: ['7'],
+        },
+        asDown: false,
+      },
+      watch: {
+        asDown() {
+          if (this.asDown) {
+            this.cards.splice(12, 1);
+            this.cards.unshift('a');
+          }
         },
       },
       methods: {
